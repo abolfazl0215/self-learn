f40d1403abe37b1c72d9e192b674b37b1786cd08
Abolfazl Mokhtari <abolfazl021mokhtari@gmail.com>
2021-06-23 18:48:51 +0430

    change font to BYekan and create part1 home-page

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -3,8 +3,11 @@ import React from 'react';
 const MainPage = () => {
     return ( 
         <div>
-            <div className="w-full h-80 bg-gray-50">
-                
+            <div className="w-full bg-gray-50 py-5">
+                <img src="image/illustration.png" alt="" className="w-72 mx-auto"/>
+                <h1 className="text-center font-semibold">آکادمی آموزش آنلاین سلف لرن</h1><br />
+                <p className="text-center text-gray-500">برنامه نویسی آسان تر از چیزی هست که فکر میکنید</p><br />
+                <div className="flex justify-center"><button className="bg-violet text-white py-2 px-5 rounded-md">نقشه راه یادگیری</button></div>
             </div>
         </div>
      );

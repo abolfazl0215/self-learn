de8ddda16141bc55eace640752b09edcf60b2fc4
Abolfazl Mokhtari <abolfazl021mokhtari@gmail.com>
2021-06-26 15:47:29 +0430

    debug homePage

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -5,9 +5,9 @@ import Course from './../Course/Course';
 const MainPage = () => {
     return ( 
         <div>
-            <div className="box-main w-full bg-gray-50 py-12 sm:grid grid-cols-4">
-                <img src="image/illustration.png" alt="" className="w-72 mx-auto sm:col-span-2 w-96"/>
-                <div className="sm:col-span-2 mt-20">
+            <div className="box-main w-full bg-gray-50 py-12 sm:grid sm:grid-cols-4">
+                <img src="image/illustration.png" alt="" className="w-72 mx-auto sm:col-span-2 lg:w-96"/>
+                <div className="lg:col-span-2 lg:mt-20">
                     <h1 className="text-center font-semibold">آکادمی آموزش آنلاین سلف لرن</h1><br />
                     <p className="text-center text-gray-500">برنامه نویسی آسان تر از چیزی هست که فکر میکنید</p><br />
                     <div className="flex justify-center"><button className="bg-violet text-white py-2 px-5 rounded-md">نقشه راه یادگیری</button></div>
@@ -21,7 +21,7 @@ const MainPage = () => {
                         <p className="w-3/4 text-center text-gray-500 mx-auto mt-2">با مربی های حرفه  ای و با حوصله رفع اشکال میکنید</p>
                     </div>
                 </div>
-                <div className="42 h-52 p-1 lg:w-1/5 lg:m-4">
+                <div className="w-1/2 h-52 p-1 lg:w-1/5 lg:m-4">
                     <div className="w-full h-full rounded pt-5 boxes">
                         <img src="image/refund.svg" alt="" className="w-8 mx-auto" />
                         <h1 className="text-center pt-2">تضمیت بازگشت وجه</h1>
@@ -50,13 +50,13 @@ const MainPage = () => {
                 <div className="w-full pt-10">
                     <h1 className="text-center"><span className="rounded-full w-4 h-4 text-violet ml-2">|</span>آخرین مقالات<span className="rounded-full w-4 h-4 text-violet mr-2">|</span></h1>
                     <div className="article pt-5 ">
-                        <div className="w-full boxes rounded p-3 lg:text-center  mx-auto m-3"><h2>فرانت اند چیه ؟</h2></div>
-                        <div className="w-full boxes rounded p-3 mt-2 lg:text-center  mx-auto m-3"><h2>چرا ری اکت ؟</h2></div>
-                        <div className="w-full boxes rounded p-3 mt-2 lg:text-center  mx-auto m-3"><h2>ورود به دنیای برنامه نویسی</h2></div>
-                        <div className="w-full boxes rounded p-3 mt-2 lg:text-center  mx-auto m-3"><h2>چگونه برنامه نویس بهتری شویم ؟</h2></div>
-                        <div className="w-full boxes rounded p-3 mt-2 lg:text-center  mx-auto m-3"><h2>sass چیست ؟</h2></div>
-                        <div className="w-full boxes rounded p-3 mt-2 lg:text-center  mx-auto m-3"><h2>flex چیست ؟</h2></div>
-                        <div className="w-full boxes rounded p-3 mt-2 lg:text-center  mx-auto m-3"><h2>grid چیست ؟</h2></div>
+                        <div className="w-full boxes rounded p-3 lg:text-center  lg:mx-auto lg:m-3"><h2>فرانت اند چیه ؟</h2></div>
+                        <div className="w-full boxes rounded p-3 mt-2 lg:text-center  lg:mx-auto lg:m-3"><h2>چرا ری اکت ؟</h2></div>
+                        <div className="w-full boxes rounded p-3 mt-2 lg:text-center  lg:mx-auto lg:m-3"><h2>ورود به دنیای برنامه نویسی</h2></div>
+                        <div className="w-full boxes rounded p-3 mt-2 lg:text-center  lg:mx-auto lg:m-3"><h2>چگونه برنامه نویس بهتری شویم ؟</h2></div>
+                        <div className="w-full boxes rounded p-3 mt-2 lg:text-center  lg:mx-auto lg:m-3"><h2>sass چیست ؟</h2></div>
+                        <div className="w-full boxes rounded p-3 mt-2 lg:text-center  lg:mx-auto lg:m-3"><h2>flex چیست ؟</h2></div>
+                        <div className="w-full boxes rounded p-3 mt-2 lg:text-center  lg:mx-auto lg:m-3"><h2>grid چیست ؟</h2></div>
                     </div>
                 </div>
 
